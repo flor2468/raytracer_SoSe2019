8cbf4007d3b6682931e5662cba9173113b18d257
Hannah Bussmann <hannah.bussmann@uni-weimar.de>
2019-07-09 16:04:57 +0200

    Aufgabe 6.4 kleine Aenderung

--- framework/material.hpp
+++ framework/material.hpp
@@ -6,10 +6,6 @@
 #include <memory> //contains shared pointer
 
 struct Material{
-    // Material();
-    // Material(std::string name_, float ka_r, float ka_g, float ka_b, float kd_r, float kd_g, float kd_b, float ks_r, float ks_g, float ks_b, float m) ;
-    //Material(std::string name_, Color ka, Color kd, Color ks, float m);
-    //Material(std::string name_, Color ka{float r, float g, float b}, Color kd{float r, float g, float b}, Color ks{float r, float g, float b}, float m);
     std::string name_ = {" "};
     Color ka; 
     Color kd; 
